84694182e70ed429d82bc48442a67c5042a27277
elvanaud <arthurlouvrier@hotmail.fr>
2025-05-04 15:10:10 +0200

    data - Renamed technologies and fixed some changes in the tech tree

--- SeaBlock/data/tech-tree.lua
+++ SeaBlock/data/tech-tree.lua
@@ -22,14 +22,14 @@ bobmods.lib.tech.remove_prerequisite("angels-iron-smelting-2", "angels-coal-proc
 bobmods.lib.tech.replace_prerequisite("angels-coal-processing-2", "water-treatment-2", "basic-chemistry-3")
 
 -- Add other prerequisites
-bobmods.lib.tech.add_prerequisite("gardens", "electronics")
+bobmods.lib.tech.add_prerequisite("gardens", "bob-electronics")
 if mods["ScienceCostTweakerM"] then
   bobmods.lib.tech.add_prerequisite("sct-bio-science-pack", "bio-arboretum-1")
   bobmods.lib.tech.add_prerequisite("sb-bio-processing-advanced", "sct-bio-science-pack")
   bobmods.lib.tech.add_prerequisite("utility-science-pack", "rubber")
 end
-bobmods.lib.tech.add_prerequisite("angels-glass-smelting-1", "silicon-processing")
-bobmods.lib.tech.add_prerequisite("angels-stone-smelting-2", "silicon-processing")
+bobmods.lib.tech.add_prerequisite("angels-glass-smelting-1", "bob-silicon-processing")
+bobmods.lib.tech.add_prerequisite("angels-stone-smelting-2", "bob-silicon-processing")
 
 -- Add missing Science Pack Tech prerequisites
 
@@ -61,12 +61,12 @@ bobmods.lib.tech.add_prerequisite("basic-chemistry-2", "logistic-science-pack")
 bobmods.lib.tech.add_prerequisite("logistic-system", "production-science-pack")
 
 -- Utility / Yellow
-bobmods.lib.tech.add_prerequisite("radars-5", "utility-science-pack")
+bobmods.lib.tech.add_prerequisite("bob-radar-5", "utility-science-pack")
 
 -- Space / White
 if mods["bobequipment"] then
   bobmods.lib.tech.add_prerequisite("bob-energy-shield-equipment-6", "space-science-pack")
-  bobmods.lib.tech.add_prerequisite("fusion-reactor-equipment-4", "space-science-pack")
+  bobmods.lib.tech.add_prerequisite("bob-fission-reactor-equipment-4", "space-science-pack")
 end
 if mods["bobwarfare"] then
   bobmods.lib.tech.add_prerequisite("bob-power-armor-5", "space-science-pack")
@@ -91,16 +91,49 @@ bobmods.lib.tech.add_prerequisite("space-science-pack", "military")
 
 -- Steam power
 bobmods.lib.tech.add_prerequisite("automation", "steam-power")
-bobmods.lib.tech.add_prerequisite("optics", "steam-power")
+bobmods.lib.tech.add_prerequisite("lamp", "steam-power")
 if data.raw.technology["bob-greenhouse"] then
   bobmods.lib.tech.add_prerequisite("bob-greenhouse", "steam-power")
 end
 bobmods.lib.tech.add_prerequisite("angels-coal-processing", "steam-power")
 
 -- Gems are needed to make higher tier modules
-if data.raw.technology["gem-processing-3"] then
+if data.raw.technology["bob-gem-processing-3"] then
   --Module with 2 dots has the tech name of 3
-  bobmods.lib.tech.add_prerequisite("speed-module-3", "gem-processing-3")
-  bobmods.lib.tech.add_prerequisite("productivity-module-3", "gem-processing-3")
-  bobmods.lib.tech.add_prerequisite("effectivity-module-3", "gem-processing-3")
+  bobmods.lib.tech.add_prerequisite("speed-module-3", "bob-gem-processing-3")
+  bobmods.lib.tech.add_prerequisite("productivity-module-3", "bob-gem-processing-3")
+  bobmods.lib.tech.add_prerequisite("efficiency-module-3", "bob-gem-processing-3")
+end
+
+
+bobmods.lib.tech.remove_prerequisite("automation-science-pack", "electronics")
+seablock.lib.hide_technology("automation-science-pack")
+
+seablock.lib.hide_technology("electronics") --new trigger tech in base game, we don't want it in seablock
+-- I think i found a bug: trigger techs still research even if hidden and disabled
+-- data.raw["technology"]["electronics"] = nil -- multiple different techs and shortcut will complain if we do that
+data.raw["technology"]["electronics"].research_trigger = nil
+data.raw["technology"]["electronics"].unit = {time = 1, count = 1, ingredients = {}}
+
+seablock.lib.hide_technology("electric-mining-drill")
+seablock.lib.hide_technology("repair-pack")
+
+bobmods.lib.tech.add_prerequisite("radar", "military")
+bobmods.lib.tech.remove_prerequisite("military", "automation-science-pack")
+bobmods.lib.tech.remove_prerequisite("gun-turret", "automation-science-pack")
+bobmods.lib.tech.remove_prerequisite("stone-wall", "automation-science-pack")
+bobmods.lib.tech.remove_prerequisite("radar", "automation-science-pack")
+bobmods.lib.tech.remove_prerequisite("automation", "automation-science-pack")
+
+
+-- Change order for esthetics of the tech tree 
+data.raw["technology"]["military"].order = "z-[military]"
+
+if data.raw["technology"]["logistics-0"] then
+  data.raw["technology"]["logistics-0"].research_trigger = nil
+  data.raw["technology"]["logistics-0"].unit = {
+    ingredients = {{"automation-science-pack", 1}},
+    time = 5,
+    count = 10
+  }
 end
