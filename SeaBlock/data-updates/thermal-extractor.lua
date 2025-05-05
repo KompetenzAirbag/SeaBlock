5d81e2f0bebcb5362e345e707963e8876ab4e14a
elvanaud <arthurlouvrier@hotmail.fr>
2025-05-05 17:20:28 +0200

    Fixed thermal extractor fluid box and locale

--- SeaBlock/data-updates/thermal-extractor.lua
+++ SeaBlock/data-updates/thermal-extractor.lua
@@ -55,14 +55,14 @@ data.raw["assembling-machine"]["thermal-extractor"] = extractor
 extractor.type = "assembling-machine"
 extractor.crafting_speed = 1
 extractor.ingredient_count = 2
-extractor.fluid_boxes = { --TODO: check validity of fluid boxes
+extractor.fluid_boxes = {
   {
     production_type = "input",
     base_area = 10,
     --base_level = -1,
     volume = 1000,
     pipe_covers = pipecoverspictures(),
-    pipe_connections = { { flow_direction = "input", position = { 3, 4.4 }, direction = defines.direction.north } },
+    pipe_connections = { { flow_direction = "input", position = { 3, - 4 }, direction = defines.direction.north } },
   },
   {
     production_type = "output",
@@ -70,7 +70,7 @@ extractor.fluid_boxes = { --TODO: check validity of fluid boxes
     --base_level = 1,
     volume = 1000,
     pipe_covers = pipecoverspictures(),
-    pipe_connections = { { flow_direction = "output", position = { -3, 4.4 }, direction = defines.direction.south } },
+    pipe_connections = { { flow_direction = "output", position = { -3, 4 }, direction = defines.direction.south } },
   },
 }
 extractor.animation = {
@@ -84,6 +84,7 @@ extractor.fixed_recipe = "thermal-extractor-water"
 bobmods.lib.tech.add_recipe_unlock("thermal-water-extraction-2", "thermal-extractor-water")
 move_item("thermal-extractor", "water-treatment-building", "f[thermal-extractor]-b[extractor]", "item")
 bobmods.lib.recipe.add_ingredient("thermal-extractor", { type = "item", name = "thermal-bore", amount = 1 })
+extractor.vector_to_place_result = nil -- remove the yellow arrow of the mining drill
 
 local bore = data.raw["mining-drill"]["thermal-bore"]
 data.raw["mining-drill"]["thermal-bore"] = nil
@@ -91,7 +92,7 @@ data.raw["assembling-machine"]["thermal-bore"] = bore
 bore.type = "assembling-machine"
 bore.crafting_speed = 1
 bore.ingredient_count = 1
-bore.fluid_boxes = { --TODO: check this
+bore.fluid_boxes = {
   {
     production_type = "output",
     base_area = 1,
@@ -101,12 +102,13 @@ bore.fluid_boxes = { --TODO: check this
     pipe_connections = {
       {
         flow_direction = "output",
-        positions = { { 1, -1.4 }, { 1.4, -1 }, { -1, 1.4 }, { -1.4, 1 } },
+        positions = { { 1, -1 }, { 1, -1 }, { -1, 1 }, { -1, 1 } },
         direction = defines.direction.north,
       },
     },
   },
 }
+bore.vector_to_place_result = nil
 
 local function makesheet(sheet, count, d)
   local r = table.deepcopy(sheet)
