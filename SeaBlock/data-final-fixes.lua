17bfedb799b28d7a6dacc2063cddbaf9899fe53e
Athena Faris <75452942+KompetenzAirbag@users.noreply.github.com>
2025-09-20 16:43:53 +0200

    Fixes hand craftable recipes

--- SeaBlock/data-final-fixes.lua
+++ SeaBlock/data-final-fixes.lua
@@ -39,5 +39,45 @@ for _, v in pairs(data.raw.character) do
   v.crafting_categories = v.crafting_categories or {}
   
   table.insert(v.crafting_categories, "sb-crafting-handonly")
-  table.insert(v.crafting_categories, "electronics")
+  table.insert(v.crafting_categories, "sb-hand-electronics")
+end
+
+local function add_recipe_category(recipe_name, category_name)
+  local recipe = data.raw.recipe[recipe_name]
+  if (not recipe) then error("Tried to set recipe category for recipe "..recipe_name..": Recipe not found") end
+
+  recipe.additional_categories = recipe.additional_categories or {}
+
+  table.insert(recipe.additional_categories, category_name)
+end
+
+-- Adds handcrafting recipes because crafting category "electronics" is no longer craftable by hand
+local handcrafting_recipes = {
+  "electronic-circuit",
+  "copper-cable",
+  "advanced-circuit",
+  "bob-tinned-copper-cable",
+  "bob-insulated-cable",
+  "bob-gilded-copper-cable",
+  "bob-wooden-board",
+  "bob-basic-circuit-board",
+  "bob-robot-brain",
+  "bob-robot-brain-2",
+  "bob-robot-brain-3",
+  "bob-robot-brain-4",
+  "bob-module-case",
+  "bob-module-contact",
+  "bob-speed-processor",
+  "bob-efficiency-processor",
+  "bob-productivity-processor",
+  "angels-wire-gold",
+  "angels-wire-platinum",
+  "angels-wire-silver",
+  "angels-wire-tin",
+  "cp-electronic-circuit-board",
+  "cp-advanced-circuit-board"
+}
+
+for _, name in pairs(handcrafting_recipes) do
+  add_recipe_category(name, "crafting")
 end
