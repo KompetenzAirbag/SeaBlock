30790060f1f91ed3aef08fe6a5ce9481b01425c3
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2025-12-08 22:59:09 +1300

    Restore check for character.crafting_categories
    This is an optional field so we need to check if it exists

--- SeaBlock/data-final-fixes.lua
+++ SeaBlock/data-final-fixes.lua
@@ -34,4 +34,7 @@ data.raw.recipe["copper-cable"].allow_decomposition = true
 data.raw.recipe["angels-solid-paper"].allow_decomposition = true
 
 for _, v in pairs(data.raw.character) do
-  table.insert(v.crafting_categories, "sb-crafting-handonly")
+  if v.crafting_categories then
+    table.insert(v.crafting_categories, "sb-crafting-handonly")
+  end
+end
