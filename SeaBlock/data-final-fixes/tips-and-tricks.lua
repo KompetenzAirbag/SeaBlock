78539eb3305edf0a7d46233f022f8aaf0b713e8a
Linus Lovelace <morallygraystrategicservices@gmail.com>
2026-06-03 07:29:31 -0400

    Remove tips for unavailable resources

---
+++ SeaBlock/data-final-fixes/tips-and-tricks.lua
@@ -0,0 +1,3 @@
+data.raw["tips-and-tricks-item"]["low-power"].simulation = nil
+data.raw["tips-and-tricks-item"]["angels-petrochem"].localised_description[3] = nil
+data.raw["tips-and-tricks-item"]["angels-bio-nauvis-1b"] = nil
